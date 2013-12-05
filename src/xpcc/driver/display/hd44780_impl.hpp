b1443c16ef1a2f3dc685c340eca91d59bd3d3238
Valmantas Palikša <walmis@balticum-tv.lt>
2013-12-05 19:10:56 +0200

    Revert to old working hd44780 code

--- src/xpcc/driver/display/hd44780_impl.hpp
+++ src/xpcc/driver/display/hd44780_impl.hpp
@@ -1,143 +1,181 @@
 // coding: utf-8
+// ----------------------------------------------------------------------------
 /* Copyright (c) 2009, Roboterclub Aachen e.V.
- * All Rights Reserved.
+ * All rights reserved.
  *
- * The file is part of the xpcc library and is released under the 3-clause BSD
- * license. See the file `LICENSE` for the full license governing this code.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Roboterclub Aachen e.V. nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ROBOTERCLUB AACHEN E.V. ''AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL ROBOTERCLUB AACHEN E.V. BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 // ----------------------------------------------------------------------------
 
-#ifndef XPCC_HD44780_HPP
+#ifndef XPCC__HD44780_HPP
 	#error	"Don't include this file directly, use 'hd44780.hpp' instead!"
 #endif
 
 // ----------------------------------------------------------------------------
-template <typename DATA, typename RW, typename RS, typename E>
-xpcc::Hd44780<DATA, RW, RS, E>::Hd44780(uint8_t width, uint8_t height) :
+
+template <typename E, typename RW, typename RS, typename DATA>
+xpcc::Hd44780<E, RW, RS, DATA>::Hd44780(uint8_t width, uint8_t height) : 
 	CharacterDisplay(width, height)
 {
 }
-
-template <typename DATA, typename RW, typename RS, typename E>
-void
-xpcc::Hd44780<DATA, RW, RS, E>::initialize()
-{
-	driver::initialize(this->lineCount == 1 ?
-			driver::LineMode::OneLine :
-			driver::LineMode::TwoLines);
-}
-
-template <typename DATA, typename RW, typename RS, typename E>
-void
-xpcc::Hd44780<DATA, RW, RS, E>::writeRaw(char c)
-{
-	while(!driver::writeRAM(static_cast<uint8_t>(c)))
-		;
-}
-
-template <typename DATA, typename RW, typename RS, typename E>
-void
-xpcc::Hd44780<DATA, RW, RS, E>::setCursor(uint8_t column, uint8_t line)
-{
-	this->column = (column <= this->lineWidth ? column : this->lineWidth);
-	this->line = (line <= this->lineCount ? line : this->lineCount);
-
-	uint8_t address = this->column + 0x40 * this->line;
-	if (this->line >= 2) {
-		address += 20;
-	}
-	while(!driver::writeAddress(address))
-		;
-}
-
-template <typename DATA, typename RW, typename RS, typename E>
-void
-xpcc::Hd44780<DATA, RW, RS, E>::execute(Command command)
-{
-	while(!driver::writeCommand(command))
-		;
-}
-
-template <typename DATA, typename RW, typename RS, typename E>
-void
-xpcc::Hd44780<DATA, RW, RS, E>::clear()
-{
-	while(!driver::clear())
-		;
-	while(!driver::resetCursor())
-		;
-}
-
 
 // ----------------------------------------------------------------------------
-template <typename DATA, typename RW, typename RS, typename E1, typename E2>
-xpcc::Hd44780Dual<DATA, RW, RS, E1, E2>::Hd44780Dual(uint8_t width, uint8_t height) :
-	CharacterDisplay(width, height)
+
+template <typename E, typename RW, typename RS, typename DATA>
+void
+xpcc::Hd44780<E, RW, RS, DATA>::initialize()
 {
+	E::setOutput();
+	RW::setOutput();
+	RS::setOutput();
+	DATA::setOutput();
+	
+	E::reset();
+	RS::reset();
+	xpcc::delay_ms(15);
+	
+	writeNibble(0x03);
+	xpcc::delay_ms(5);
+	
+	writeNibble(0x03);
+	xpcc::delay_us(100);
+	
+	writeNibble(0x03);
+	xpcc::delay_us(100);
+	
+	writeNibble(0x02);
+	xpcc::delay_us(100);
+	
+	writeCommand(0x28);		// 2 lines 5*7
+	writeCommand(0x08);		// display off
+	writeCommand(0x01);		// display clear
+	writeCommand(0x06);		// cursor increment
+	writeCommand(0x0C);		// on, no cursor, no blink
 }
 
-template <typename DATA, typename RW, typename RS, typename E1, typename E2>
+template <typename E, typename RW, typename RS, typename DATA>
 void
-xpcc::Hd44780Dual<DATA, RW, RS, E1, E2>::initialize()
+xpcc::Hd44780<E, RW, RS, DATA>::writeRaw(char c)
 {
-	driver1::initialize(driver1::LineMode::TwoLines);
-	driver2::initialize(driver2::LineMode::TwoLines);
+	waitBusy();
+	
+	DATA::setOutput();
+	RW::reset();
+	RS::set();
+	
+	uint8_t data = c;
+	writeNibble(data >> 4);
+	writeNibble(data);
 }
 
-template <typename DATA, typename RW, typename RS, typename E1, typename E2>
+template <typename E, typename RW, typename RS, typename DATA>
 void
-xpcc::Hd44780Dual<DATA, RW, RS, E1, E2>::writeRaw(char c)
+xpcc::Hd44780<E, RW, RS, DATA>::setCursor(uint8_t column, uint8_t line)
 {
-	if (this->line < 2) {
-		while(!driver1::writeRAM(static_cast<uint8_t>(c)))
-			;
-	} else {
-		while(!driver2::writeRAM(static_cast<uint8_t>(c)))
-			;
+	this->column = column;
+	this->line = line;
+	
+	column += 0x40 * line;
+	if (line >= 2) {
+		column += 20;
 	}
+	writeCommand(0x80 | column);
 }
 
-template <typename DATA, typename RW, typename RS, typename E1, typename E2>
+template <typename E, typename RW, typename RS, typename DATA>
 void
-xpcc::Hd44780Dual<DATA, RW, RS, E1, E2>::setCursor(uint8_t column, uint8_t line)
+xpcc::Hd44780<E, RW, RS, DATA>::execute(Command command)
 {
-	this->column = (column <= this->lineWidth ? column : this->lineWidth);
-	this->line = (line <= this->lineCount ? line : this->lineCount);
+	writeCommand(command);
+}
 
-	uint8_t address = this->column;
-	if (this->line & 0x01) {
-		address += 0x40;
+// ----------------------------------------------------------------------------
+template <typename E, typename RW, typename RS, typename DATA>
+void
+xpcc::Hd44780<E, RW, RS, DATA>::writeNibble(uint8_t data)
+{
+	DATA::write(data);
+	
+	E::set();
+	xpcc::delay_us(1);
+	E::reset();
+}
+
+template <typename E, typename RW, typename RS, typename DATA>
+uint8_t
+xpcc::Hd44780<E, RW, RS, DATA>::readByte()
+{
+	uint8_t data;
+	
+	DATA::setInput();
+	RS::reset();
+	RW::set();
+	
+	E::set();
+	xpcc::delay_us(1);
+	data = DATA::read();
+	E::reset();
+	
+	data <<= 4;
+	
+	E::set();
+	xpcc::delay_us(1);
+	data |= DATA::read();
+	E::reset();
+	
+	return data;
+}
+
+template <typename E, typename RW, typename RS, typename DATA>
+void
+xpcc::Hd44780<E, RW, RS, DATA>::waitBusy()
+{
+	while (readByte() & 0x80) {
+		// wait until busy flag is reseted
 	}
-
-	if (this->line < 2) {
-		while(!driver1::writeAddress(address))
-			;
-	} else {
-		while(!driver2::writeAddress(address))
-			;
-	}
+	
+	// the address counter is updated 4 us after the busy flag is reseted
+	xpcc::delay_us(2);
 }
 
-template <typename DATA, typename RW, typename RS, typename E1, typename E2>
+template <typename E, typename RW, typename RS, typename DATA>
 void
-xpcc::Hd44780Dual<DATA, RW, RS, E1, E2>::execute(Command command)
+xpcc::Hd44780<E, RW, RS, DATA>::clear()
 {
-	while(!driver1::writeCommand(command))
-		;
-	while(!driver2::writeCommand(command))
-		;
+	writeCommand(0x01);
 }
 
-template <typename DATA, typename RW, typename RS, typename E1, typename E2>
+template <typename E, typename RW, typename RS, typename DATA>
 void
-xpcc::Hd44780Dual<DATA, RW, RS, E1, E2>::clear()
+xpcc::Hd44780<E, RW, RS, DATA>::writeCommand(uint8_t command)
 {
-	while(!driver1::clear())
-		;
-	while(!driver1::resetCursor())
-		;
-	while(!driver2::clear())
-		;
-	while(!driver2::resetCursor())
-		;
+	waitBusy();
+	
+	DATA::setOutput();
+	RW::reset();
+	RS::reset();
+	
+	writeNibble(command >> 4);
+	writeNibble(command);
 }
