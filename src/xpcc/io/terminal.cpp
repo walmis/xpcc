3096ea72104805dd331d3034c0df8f64435af632
walmis <walmis@balticum-tv.lt>
2014-04-07 10:58:03 +0300

    lpc17 i2c driver update

--- src/xpcc/io/terminal.cpp
+++ src/xpcc/io/terminal.cpp
@@ -11,7 +11,78 @@
 
 namespace xpcc {
 
+// To convert a-f or A-F to a decimal number
+static int chartoint(int c)
+{
+    char hex[] = "aAbBcCdDeEfF";
+    int i;
+    int result = 0;
+
+    for(i = 0; result == 0 && hex[i] != '\0'; i++)
+    {
+        if(hex[i] == c)
+        {
+            result = 10 + (i / 2);
+        }
+    }
+
+    return result;
+}
+
+static unsigned int htoi(const char s[])
+{
+    unsigned int result = 0;
+    int i = 0;
+    int proper = 1;
+    int temp;
+
+    //To take care of 0x and 0X added before the hex no.
+    if(s[i] == '0')
+    {
+        ++i;
+        if(s[i] == 'x' || s[i] == 'X')
+        {
+            ++i;
+        }
+    }
+
+    while(proper && s[i] != '\0')
+    {
+        result = result * 16;
+        if(s[i] >= '0' && s[i] <= '9')
+        {
+            result = result + (s[i] - '0');
+        }
+        else
+        {
+            temp = chartoint(s[i]);
+            if(temp == 0)
+            {
+                proper = 0;
+            }
+            else
+            {
+                result = result + temp;
+            }
+        }
+
+        ++i;
+    }
+    //If any character is not a proper hex no. ,  return 0
+    if(!proper)
+    {
+        result = 0;
+    }
+
+    return result;
+}
+
 int Terminal::to_int(char *p) {
+
+	if(p[0] == '0' && (p[1] == 'x' || p[1] == 'X')) {
+		return htoi(p);
+	}
+
 	int k = 0;
 	bool neg = false;
 	if (*p == '-') {
