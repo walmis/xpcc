5978f59ca82bec66d70420174b504e65233582e1
Valmantas Paliksa <walmis@gmail.com>
2018-07-20 21:22:58 +0300

    remove custom buggy strtok

--- src/xpcc/architecture/platform/common/syscalls.cpp
+++ src/xpcc/architecture/platform/common/syscalls.cpp
@@ -302,34 +302,6 @@ double atof(const char* c) {
 }
 
 extern "C"
-char* strtok(char *str, const char* delim) {
-    static char* _buffer;
-    if(str != NULL) _buffer = str;
-    if(_buffer[0] == '\0') return NULL;
-
-    char *ret = _buffer, *b;
-    const char *d;
-
-    for(b = _buffer; *b !='\0'; b++) {
-        for(d = delim; *d != '\0'; d++) {
-            if(*b == *d) {
-                *b = '\0';
-                _buffer = b+1;
-
-                // skip the beginning delimiters
-                if(b == ret) {
-                    ret++;
-                    continue;
-                }
-                return ret;
-            }
-        }
-    }
-
-    return ret;
-}
-
-extern "C"
 void* calloc(size_t nmemb, size_t size) {
    if(!nmemb) return 0;
    void* ptr = malloc(nmemb*size);
